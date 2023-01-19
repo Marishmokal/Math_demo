1e3bb22be04ad0aeb711a99f14003bb6587b78f5
Marishmokal <marishmokal96@gmail.com>
2023-01-19 08:31:05 +0530

    first commit

---
+++ Math_demo/src/main/java/pro/wings/math/MathDemo.java
@@ -0,0 +1,17 @@
+package pro.wings.math;
+
+import pro.cal.CalculatorDemo;
+
+public class MathDemo {
+public static void main(String[] args) {
+	
+	System.out.println(sum(10,20));
+}
+public static int sum(int m1,int m2)
+{
+
+	 CalculatorDemo c=new CalculatorDemo();
+	   return m1+m2;
+	
+}
+}
